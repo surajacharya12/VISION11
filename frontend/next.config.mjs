3ac2bf0ee73c316acc2d80d08f61a03c85a95930
surajacharya12 <surajacharya.080@kathford.edu.np>
2026-08-13 11:38:45 +0545

    final

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -86,6 +86,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'i.insider.com',
+                port: '',
+                pathname: '/**',
+            },
         ],
 
     },
